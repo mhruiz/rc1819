2afc9524c751fea255977109399ac1929fe2576a
Miguel <migue.herrera272@alu.uhu.es>
2019-03-05 14:19:07 +0100

    Programa persona prolog

---
+++ persona.pl
@@ -0,0 +1,22 @@
+
+% hombre(Persona)
+% es cierto si Persona es un hombre
+
+hombre(luis).
+hombre(antonio).
+hombre(jose).
+
+% persona(P)
+% es cierto P es hombre o P es mujer
+
+% Si hombre(P) -> persona(P)
+persona(P) :- hombre(P).
+
+% Si mujer(P) -> persona(P)                             
+persona(P) :- mujer(P).
+
+% mujer(Persona)
+% es cierto si Persona es mujer
+mujer(maria).
+mujer(luisa).
+mujer(marta).
